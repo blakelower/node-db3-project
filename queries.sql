238dd03b1a597c2bc8dab56415523676de02d4c0
blakelower <blakelower1999@gmail.com>
2020-04-15 12:50:16 -0700

    was able to get queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,16 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT p.ProductName,
+c.categoryName
+FROM Product as p
+Join Category as c ON p.CategoryId = c.ID
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+SELECT CompanyName, [Order].Id
+FROM [Order]
+JOIN Shipper ON Shipper.Id = [Order].ShipVia
+WHERE [Order].OrderDate < "2012-08-09"
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -17,8 +17,11 @@ function findById(id) {
   return db("schemes").where({ id }).first();
 }
 
-function findSteps() {
-  return db("schemes");
+function findSteps(userId){
+    return db("steps")
+    .join("schemes" , "schemes.id", "steps.scheme_id")
+    .where("schemes.id", userId)
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions ")
 }
 
 function add(scheme) {
