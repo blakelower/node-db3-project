b71ed9e5d0d69b6393bf12dbb2300e3b091fcc25
blakelower <blakelower1999@gmail.com>
2020-04-15 12:50:49 -0700

    sql

--- queries.sql
+++ queries.sql
@@ -12,5 +12,9 @@ FROM [Order]
 JOIN Shipper ON Shipper.Id = [Order].ShipVia
 WHERE [Order].OrderDate < "2012-08-09"
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT ProductName, Quantity
+FROM Product
+JOIN OrderDetail on Product.Id = OrderDetail.ProductId
+WHERE OrderDetail.OrderId = "10251"
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
